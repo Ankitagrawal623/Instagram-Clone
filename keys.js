044e5f7b131163b27bcbcd9d0db6b18b0df8d96d
Ankitagrawal623 <Ankitagrawal623@gmail.com>
2021-04-01 15:27:14 +0530

    first-commit

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI: "mongodb+srv://AnkitAgarwal:u5289m4jyesOKtKN@cluster0.smuww.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    JWT_SECRET:"hchfchvvutccuiyjjia"
+}
